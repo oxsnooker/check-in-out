e935f8c9b46e6f6f451e800bb3935c30f8f16c6e
afzal afu <Afzalafu76@gmail.com>
2025-12-23 16:22:10 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,8 @@
-// Firebase config is not available as Firestore has been removed.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-3906181085-6b916",
+  "appId": "1:1057779784866:web:c1624b66161e6cef6b8bfb",
+  "apiKey": "AIzaSyC47S9OsVT7oeaV5l9wgGSFeBGddKmvfRk",
+  "authDomain": "studio-3906181085-6b916.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1057779784866"
+};
